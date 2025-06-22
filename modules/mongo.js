79604a32202e12591cb12ebd9d56bdcd40a1e86e
ownl3fans <codevoicex@gmail.com>
2025-06-23 02:07:16 +0300

    Update mongo.js

--- modules/mongo.js
+++ modules/mongo.js
@@ -2,6 +2,10 @@
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error('[MongoDB] Не задана переменная окружения MONGODB_URI! Соединение с базой невозможно.');
+  throw new Error('MONGODB_URI is not set');
+}
 const dbName = 'apgrhost';
 const collectionName = 'visitors';
 
@@ -11,11 +15,17 @@ let collection;
 
 async function connectMongo() {
   if (collection) return collection;
-  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  await client.connect();
-  db = client.db(dbName);
-  collection = db.collection(collectionName);
-  return collection;
+  try {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    db = client.db(dbName);
+    collection = db.collection(collectionName);
+    console.log('[MongoDB] Успешное подключение к базе данных!');
+    return collection;
+  } catch (err) {
+    console.error('[MongoDB] Ошибка подключения:', err);
+    throw err;
+  }
 }
 
 async function saveVisitor(visitId, data) {
